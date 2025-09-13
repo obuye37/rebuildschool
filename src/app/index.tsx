deabbb7422a53ae9f5c3d94c4e934e254404c575
obuye37 <bomajehori@gmail.com>
2025-09-13 21:34:33 +0100

    first rebuild

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,6 @@ export * from "./layout";
 export * from "./page";
 export * from "./our-stats";
 export * from "./about-event";
-export * from "./faq";
 export * from "./sponsored-by";
 export * from "./event-content";
 
