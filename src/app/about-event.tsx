99bd6564dc1ee4d2b4e9a154ac7cd605ee8c3ec0
obuye37 <bomajehori@gmail.com>
2025-09-14 03:20:03 +0100

    major update

--- src/app/about-event.tsx
+++ src/app/about-event.tsx
@@ -5,14 +5,12 @@ import { Typography } from "@material-tailwind/react";
 export function AboutEvent() {
   return (
     <section className="container mx-auto flex flex-col items-center sm:px-40 sm:py-50 px-8 py-16">
-      <Typography variant="h3" className="text-center mb-2 text-gray-800 " placeholder={undefined} onResize={undefined} onResizeCapture={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+      <div  className="text-center mb-2 text-gray-800 " >
         Our Mission
-      </Typography>
-      <Typography
-        variant="lead"
-        className="mt-2 lg:max-w-4xl mb-8 w-full text-center font-normal !text-gray-700" placeholder={undefined} onResize={undefined} onResizeCapture={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}      >
+      </div>
+      <div className="mt-2 lg:max-w-4xl mb-8 w-full text-center font-normal !text-gray-700" >
         Our mission is to bring quality education to kids in Rumuodogo community school by helping to rebuild the school and supporting the students with educational supplies.
-      </Typography>
+      </div>
       {/*<div className="mt-8 w-full grid grid-cols-1 md:grid-cols-2 gap-4 ">
          <div>
           <AboutCard
