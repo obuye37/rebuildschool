62036a7c7c587a0dfd1b007093e8339e9b138592
obuye37 <bomajehori@gmail.com>
2025-09-14 21:16:48 +0100

    major update

--- src/app/how-it-works.tsx
+++ src/app/how-it-works.tsx
@@ -7,7 +7,7 @@ const HowItWorks = () => {
         <img src="/image/image-4.jpg" alt="How it works" className="mx-auto object-cover w-full md:h-full h-[15rem]" /> 
       </div>
       <div className='flex-col h-full md:w-1/2 w-full justify-center items-center overflow-hidden p-10'>
-        <h2 className='text-4xl text-center font-bold mb-4'>How it Works</h2>
+        <h2 className='text-4xl text-center font-bold mb-4'>Transparency & Proof of Work</h2>
         <div className='text-lg text-center'>
           <p>Everything and all efforts to rebuild the school will be done live on stream. We will be visiting the school everyday to live stream the rebuilding process and also to see how these kids learn daily. Join us on a journey into the lives of kids in Rumuodogo Community School.</p>
           <br />
--- src/components/contact-us.tsx
+++ src/components/contact-us.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div className='bg-green-950 text-gray-100 flex flex-col justify-center items-center text-center p-8 gap-4'>
+      <p className='w-full text-center text-2xl p-5'>Contact Us:</p>
       <p>X:  <Link href="https://x.com/Rebuild_School">https://x.com/Rebuild_School</Link></p>
       <p> <span className='font-bold text-green-500'>REBUILD</span> is live every day  on <span className='font-bold'>PUMPFUN</span> stream.</p>
     </div>
