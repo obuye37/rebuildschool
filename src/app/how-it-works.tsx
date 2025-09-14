c68f306919d0468a4d5d5ffd8b6888f07ff00ed5
obuye37 <bomajehori@gmail.com>
2025-09-14 21:24:33 +0100

    major update

--- src/app/how-it-works.tsx
+++ src/app/how-it-works.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const HowItWorks = () => {
   return (
-    <div className='md:flex flex-col items-center gap-4'>
+    <div className='md:flex  items-center gap-4'>
       <div className='md:w-1/2 w-full overflow-hidden'>
         <img src="/image/image-4.jpg" alt="How it works" className="mx-auto object-cover w-full md:h-full h-[15rem]" /> 
       </div>
       <div className='flex-col h-full md:w-1/2 w-full justify-center items-center overflow-hidden p-10'>
-        <h2 className='text-4xl text-center font-bold mb-4'>Transparency & Proof of Work</h2>
+        <h2 className='text-2xl md:text-4xl text-center font-bold mb-4'>Transparency & Proof of Work</h2>
         <div className='text-lg text-center'>
           <p>Everything and all efforts to rebuild the school will be done live on stream. We will be visiting the school everyday to live stream the rebuilding process and also to see how these kids learn daily. Join us on a journey into the lives of kids in Rumuodogo Community School.</p>
           <br />
