002a891829fc5a29b4d8c73cfdf01eda98dafd31
obuye37 <bomajehori@gmail.com>
2025-09-14 01:13:46 +0100

    major update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,6 @@ import { Footer, Navbar } from "@/components";
 
 // sections
 import Hero from "./hero";
-import SponsoredBy from "./sponsored-by";
-import AboutEvent from "./about-event";
-import OurStats from "./our-stats";
-import EventContent from "./event-content";
 
 export default function Portfolio() {
   return (
